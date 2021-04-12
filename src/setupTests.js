506cc0fde7e28cd7e27ba1c44ef24247fcc106ff
SandeepKel <sandeep.kel@outlook.com>
2021-04-12 21:23:16 +0800

    this is to test develop branch

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+// test develop branch 
 import '@testing-library/jest-dom';
